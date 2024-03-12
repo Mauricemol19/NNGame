b7db513cc2ba49e1ef84ec494afdfb0cf9c07343
Maurice Mol <mauricemol@hotmail.nl>
2024-03-12 06:39:03 +0100

    Added Screens, Basic foundation work, added partial screenloader class and test files

---
+++ NNGame/Classes/ScreenLoader.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Screens.Transitions;
+using MonoGame.Extended.Screens;
+using NNGame.Classes.Screens;
+
+namespace NNGame.Classes
+{
+    internal class ScreenLoader
+    {
+        private readonly Main Main;
+        private readonly ScreenManager ScreenManager;
+        private readonly GraphicsDevice GraphicsDevice;
+
+        public ScreenLoader(Main _main, ScreenManager _screenManager, GraphicsDevice _graphicsDevice)
+        {
+            Main = _main;
+            ScreenManager = _screenManager; 
+            GraphicsDevice = _graphicsDevice;
+
+            LoadMenu();
+        }
+
+        public void LoadMenu()
+        {
+            ScreenManager.LoadScreen(new Menu(Main), new FadeTransition(GraphicsDevice, Color.Black));
+        }
+
+        public void LoadMap()
+        {
+
+        }
+
+        private void LoadTiledMap() 
+        {
+            //_tiledMap = Content.Load<TiledMap>("Tilemaps/Grass");
+            //_tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
+            //_spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+    }
+}
---
+++ NNGame/Classes/Screens/Menu.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended.Screens;
+using MonoGame.Extended;
+using System.Reflection.PortableExecutable;
+
+namespace NNGame.Classes.Screens
+{
+    public class Menu : GameScreen
+    {
+        private readonly GraphicsDeviceManager _graphicsDeviceManager;
+
+        private Main main => (Main)base.Game;
+
+        private Texture2D _logo;
+        private SpriteFont _font;
+        private Vector2 _position = new(50, 50);
+
+        public Menu(Main main) : base(main) { }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            _font = main.Content.Load<SpriteFont>("Fonts/font");
+            _logo = main.Content.Load<Texture2D>("Sprites/test");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _position = Vector2.Lerp(_position, Mouse.GetState().Position.ToVector2(), 1f * gameTime.GetElapsedSeconds());
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            main.GraphicsDevice.Clear(new Color(16, 139, 204));
+            main._spriteBatch.Begin();
+            main._spriteBatch.DrawString(_font, nameof(Menu), new Vector2(10, 10), Color.White);
+            main._spriteBatch.Draw(_logo, _position, Color.White);
+            main._spriteBatch.End();            
+        }
+    }
+}
